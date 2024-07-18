290e5c520b897f604199b2b33cf79f57876fc318
Sambasiva-Rao13 <158269283+Sambasiva-Rao13@users.noreply.github.com>
2024-07-18 17:52:06 +0000

    Publish to GitHub

--- TASK 4.py
+++ TASK 4.py
@@ -3,6 +3,8 @@ import random
 def play_game():
     choices = ["rock", "paper", "scissors"]
     computer_choice = random.choice(choices)
+    your_score=0
+    computer_score=0
 
     user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
     while user_choice not in choices:
@@ -16,13 +18,18 @@ def play_game():
          (user_choice == "scissors" and computer_choice == "paper") or \
          (user_choice == "paper" and computer_choice == "rock"):
         print("You win!")
+        your_score+=1
+        print(f"your score is {your_score} and computer score is {computer_score}")
     else:
         print("Computer wins!")
+        computer_score+=1
+        print(f"your score is {your_score} and computer score is {computer_score}")
 
     play_again = input("Play again? (y/n): ").lower()
     if play_again == "y":
         play_game()
     else:
         print("Thanks for playing!")
+        print(f"your score is {your_score} and computer score is {computer_score}")
 
 play_game()
